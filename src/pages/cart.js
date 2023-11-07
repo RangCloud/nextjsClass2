931e42229b1b62dd71d887ece501969758782f9a
RangCloud <mtr008@naver.com>
2023-11-07 14:30:30 +0900

    231107

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,6 +1,6 @@
 import { useContext } from "react";
-import cartContext from '../component/cartContext';
-import data from '../data/items';
+import cartContext from '../../components/cartContext';
+import data from '../../data/items';
 
 function getFullItem(id){
     const idx = data.findIndex( (item)=> item.id === id);
@@ -12,11 +12,11 @@ function Cart(){
     const {items} = useContext(cartContext);
     const total = Object.keys(items).map((id)=>getFullItem(id).price * items[id]).reduce((x,y) => x+y, 0)
 
-    const amounts = Object.keys(items).map((id)=>{getFullItem(id); return{item, amount: items[id]}})
+    const amounts = Object.keys(items).map((id)=>{const item = getFullItem(id); return{item, amount: items[id]}})
 
     return(
         <div>
-            <h1 className="">Total: ${total}</h1>
+            <h1 className="font-bold">Total: ${total}</h1>
             <div>
                 {
                     amounts.map(
